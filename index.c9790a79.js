7a2c48d421c81bcd33c237f0bbc4fae2ace48ee5
LabPetro <lamppetro30@outlook.com>
2024-01-16 06:15:09 +0200

    make build

---
+++ index.c9790a79.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(" .wall "),t=document.querySelector(" .spider "),l=(e.clientWidth-Number(t.width))/2,i=(e.clientHeight-Number(t.height))/2;t.style.position="absolute",t.style.left=l+"px",t.style.top=i+"px";
+//# sourceMappingURL=index.c9790a79.js.map
